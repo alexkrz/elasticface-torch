af7088b930270f9121677af219ad0cfc5da5f33a
Alexander Kurz <alex.kurz95@gmail.com>
2025-06-26 14:30:36 +0200

    Add training loop

--- train.py
+++ train.py
@@ -4,18 +4,22 @@ import time
 from dataclasses import dataclass
 
 import torch
+import torch.nn.functional as F
 from jsonargparse import CLI
 from torch.nn import CrossEntropyLoss
+from torch.nn.utils import clip_grad_norm_
 from torch.utils.data import DataLoader
 
 from backbones.iresnet import iresnet50, iresnet100
 from utils import losses
 from utils.dataset import HFDataset
-from utils.utils_logging import init_logging
+from utils.utils_callbacks import CallBackLogging, CallBackModelCheckpoint
+from utils.utils_logging import AverageMeter, init_logging
 
 
 @dataclass
 class Config:
+    # TODO: Make config conditional depending on dataset and loss
     dataset: str = "webface"
     data_p: str = os.environ["DATASET_DIR"] + "/TrainDatasets/parquet-files/casia_webface.parquet"
     output_p: str = "output/arcface_r50"
@@ -23,17 +27,21 @@ class Config:
     network: str = "iresnet50"
     use_se: bool = False
     embedding_size: int = 512
+    loss: str = "ArcFace"
     num_classes: int = 10572
     s: float = 64.0
     m: float = 0.50
     lr: float = 0.1
     weight_decay: float = 5e-4
     warmup_epoch: int = -1
+    num_epoch: int = 26
+    global_step: int = 0
 
 
 def main(cfg: Config):
     rank = 0
     world_size = 1
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     if not os.path.exists(cfg.output_p) and rank == 0:
         os.makedirs(cfg.output_p)
@@ -43,11 +51,10 @@ def main(cfg: Config):
     log_root = logging.getLogger()
     init_logging(log_root, rank, cfg.output_p)
 
-    train_dataset = HFDataset(cfg.data_p)
-    print(train_dataset[0][0].shape)
+    trainset = HFDataset(cfg.data_p)
 
     train_loader = DataLoader(
-        train_dataset,
+        trainset,
         batch_size=cfg.batch_size,
         num_workers=0,
         pin_memory=True,
@@ -63,18 +70,17 @@ def main(cfg: Config):
         backbone = None
         logging.info("load backbone failed!")
         exit()
+    backbone.to(device)
     backbone.train()
 
     # get header
     if cfg.loss == "ArcFace":
         header = losses.ArcFace(
-            in_features=cfg.embedding_size,
-            out_features=cfg.num_classes,
-            s=cfg.s,
-            m=cfg.m,
+            in_features=cfg.embedding_size, out_features=cfg.num_classes, s=cfg.s, m=cfg.m
         )
     else:
         print("Header not implemented")
+    header.to(device)
     header.train()
 
     opt_backbone = torch.optim.SGD(
@@ -107,6 +113,54 @@ def main(cfg: Config):
     scheduler_header = torch.optim.lr_scheduler.LambdaLR(
         optimizer=opt_header, lr_lambda=lr_step_func
     )
+
+    criterion = CrossEntropyLoss()
+
+    start_epoch = 0
+    total_step = int(len(trainset) / cfg.batch_size / world_size * cfg.num_epoch)
+    if rank == 0:
+        logging.info("Total Step is: %d" % total_step)
+
+    callback_logging = CallBackLogging(
+        50,
+        rank,
+        total_step,
+        cfg.batch_size,
+        world_size,
+        writer=None,
+    )
+    callback_checkpoint = CallBackModelCheckpoint(rank, cfg.output_p)
+
+    loss = AverageMeter()
+    global_step = cfg.global_step
+    for epoch in range(start_epoch, cfg.num_epoch):
+        for _, (img, label) in enumerate(train_loader):
+            global_step += 1
+            img = img.to(device)
+            label = label.to(device)
+
+            features = F.normalize(backbone(img))
+
+            thetas = header(features, label)
+            loss_v = criterion(thetas, label)
+            loss_v.backward()
+
+            clip_grad_norm_(backbone.parameters(), max_norm=5, norm_type=2)
+
+            opt_backbone.step()
+            opt_header.step()
+
+            opt_backbone.zero_grad()
+            opt_header.zero_grad()
+
+            loss.update(loss_v.item(), 1)
+
+            callback_logging(global_step, loss, epoch)
+
+        scheduler_backbone.step()
+        scheduler_header.step()
+
+        callback_checkpoint(global_step, backbone, header)
 
 
 if __name__ == "__main__":
---
+++ utils/utils_callbacks.py
@@ -0,0 +1,81 @@
+import logging
+import os
+import time
+
+import torch
+
+from utils.utils_logging import AverageMeter
+
+
+class CallBackLogging(object):
+    def __init__(
+        self,
+        frequent,
+        rank,
+        total_step,
+        batch_size,
+        world_size,
+        writer=None,
+        resume=0,
+        rem_total_steps=None,
+    ):
+        self.frequent: int = frequent
+        self.rank: int = rank
+        self.time_start = time.time()
+        self.total_step: int = total_step
+        self.batch_size: int = batch_size
+        self.world_size: int = world_size
+        self.writer = writer
+        self.resume = resume
+        self.rem_total_steps = rem_total_steps
+
+        self.init = False
+        self.tic = 0
+
+    def __call__(self, global_step, loss: AverageMeter, epoch: int):
+        if self.rank == 0 and global_step > 0 and global_step % self.frequent == 0:
+            if self.init:
+                try:
+                    speed: float = self.frequent * self.batch_size / (time.time() - self.tic)
+                    speed_total = speed * self.world_size
+                except ZeroDivisionError:
+                    speed_total = float("inf")
+
+                time_now = (time.time() - self.time_start) / 3600
+                # TODO: resume time_total is not working
+                if self.resume:
+                    time_total = time_now / ((global_step + 1) / self.rem_total_steps)
+                else:
+                    time_total = time_now / ((global_step + 1) / self.total_step)
+                time_for_end = time_total - time_now
+                if self.writer is not None:
+                    self.writer.add_scalar("time_for_end", time_for_end, global_step)
+                    self.writer.add_scalar("loss", loss.avg, global_step)
+                msg = (
+                    "Speed %.2f samples/sec   Loss %.4f Epoch: %d   Global Step: %d   Required: %1.f hours"
+                    % (speed_total, loss.avg, epoch, global_step, time_for_end)
+                )
+                logging.info(msg)
+                loss.reset()
+                self.tic = time.time()
+            else:
+                self.init = True
+                self.tic = time.time()
+
+
+class CallBackModelCheckpoint(object):
+    def __init__(self, rank, output="./"):
+        self.rank: int = rank
+        self.output: str = output
+
+    def __call__(self, global_step, backbone: torch.nn.Module, header: torch.nn.Module = None):
+        if global_step > 100 and self.rank == 0:
+            torch.save(
+                backbone.module.state_dict(),
+                os.path.join(self.output, str(global_step) + "backbone.pth"),
+            )
+        if global_step > 100 and header is not None:
+            torch.save(
+                header.module.state_dict(),
+                os.path.join(self.output, str(global_step) + "header.pth"),
+            )
