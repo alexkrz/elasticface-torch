a550aa0caee7462555d0ae3b16fa2c7730d898a5
Alexander Kurz <alex.kurz95@gmail.com>
2025-07-03 14:28:54 +0200

    Add verification script

--- utils/utils_callbacks.py
+++ utils/utils_callbacks.py
@@ -1,10 +1,58 @@
 import logging
 import os
 import time
+from typing import List
 
 import torch
 
+from eval import verification
 from utils.utils_logging import AverageMeter
+
+
+class CallBackVerification(object):
+    def __init__(self, frequent, rank, val_targets, rec_prefix, image_size=(112, 112)):
+        self.frequent: int = frequent
+        self.rank: int = rank
+        self.highest_acc: float = 0.0
+        self.highest_acc_list: List[float] = [0.0] * len(val_targets)
+        self.ver_list: List[object] = []
+        self.ver_name_list: List[str] = []
+        if self.rank == 0:
+            self.init_dataset(val_targets=val_targets, data_dir=rec_prefix, image_size=image_size)
+
+    def ver_test(self, backbone: torch.nn.Module, global_step: int):
+        results = []
+        for i in range(len(self.ver_list)):
+            acc1, std1, acc2, std2, xnorm, embeddings_list = verification.test(
+                self.ver_list[i], backbone, 10, 10
+            )
+            logging.info("[%s][%d]XNorm: %f" % (self.ver_name_list[i], global_step, xnorm))
+            logging.info(
+                "[%s][%d]Accuracy-Flip: %1.5f+-%1.5f"
+                % (self.ver_name_list[i], global_step, acc2, std2)
+            )
+            if acc2 > self.highest_acc_list[i]:
+                self.highest_acc_list[i] = acc2
+            logging.info(
+                "[%s][%d]Accuracy-Highest: %1.5f"
+                % (self.ver_name_list[i], global_step, self.highest_acc_list[i])
+            )
+            results.append(acc2)
+
+    def init_dataset(self, val_targets, data_dir, image_size):
+        for name in val_targets:
+            path = os.path.join(data_dir, name + ".bin")
+            if os.path.exists(path):
+                # TODO: Replace load_bin by load_data_fs
+                data_set = verification.load_bin(path, image_size)
+                self.ver_list.append(data_set)
+                self.ver_name_list.append(name)
+
+    def __call__(self, num_update, backbone: torch.nn.Module):
+        if self.rank == 0 and num_update > 0 and num_update % self.frequent == 0:
+            backbone.eval()
+            self.ver_test(backbone, num_update)
+            backbone.train()
 
 
 class CallBackLogging(object):
